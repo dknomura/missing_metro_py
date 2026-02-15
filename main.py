359c1ed9173f8730e96f70f23f1a80dc6b126224
dn <dknomura@gmail.com>
2026-02-14 17:22:42 -0800

    test linter

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from missing-metro-py!")
-
-
-if __name__ == "__main__":
-    main()
--- scripts/la_map.py
+++ scripts/la_map.py
@@ -15,11 +15,13 @@
 # %%
 import folium
 
-m = folium.Map(location=[45.5236, -122.6750], zoom_start=13) # Creates a map object
-folium.Marker(location=[45.5236, -122.6750], popup="Portland").add_to(m) # Adds a marker
+m = folium.Map(location=[45.5236, -122.6750], zoom_start=13)  # Creates a map object
+folium.Marker(location=[45.5236, -122.6750], popup="Portland").add_to(
+    m
+)  # Adds a marker
 
 m
 # %%
-print('helljo')
+print("helljo")
 
 # %%
